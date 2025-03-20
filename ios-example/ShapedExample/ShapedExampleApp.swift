298541b173eff202bd8dff9c432cfb9f1eb4b967
Cesar Nalio <cesar@nalio.com.br>
2025-03-20 12:45:36 -0300

    [Plugin IOS] App Exemplo plugin IOS

---
+++ ios-example/ShapedExample/ShapedExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapedExampleApp.swift
+//  ShapedExample
+//
+//  Created by Cesar Nalio on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapedExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
